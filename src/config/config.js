e47f90c3114c87adb3144125f41f21dfb31eff86
EYadroshnikov <yadegor1375@gmail.com>
2024-02-21 10:38:51 +0300

    fix: added DocDateTime -> ofd.dto, added example.env

--- src/config/config.js
+++ src/config/config.js
@@ -1,4 +1,15 @@
 import 'dotenv/config';
+
+/**
+ * Configuration settings for the application.
+ *
+ * @property {string} env - The environment in which the application is running (e.g., 'PROD', 'DEV').
+ * @property {string} apiUrl - The base URL for API requests, determined by the environment.
+ *
+ * @example
+ * import { config } from './config';
+ * console.log(config.apiUrl); // Outputs the API URL based on the current environment
+ */
 export const config = {
     env: process.env.ENV,
     apiUrl: process.env.ENV === 'PROD' ? process.env.API_URL : process.env.DEV_API_URL
--- src/dtos/ofd.dto.js
+++ src/dtos/ofd.dto.js
@@ -1,45 +1,45 @@
 /**
- * Data Transfer Object (DTO) for OFD records.
- * Represents the structure of an OFD record with various financial sums.
+ * Represents a Data Transfer Object (DTO) for OFD transactions.
+ *
+ * @property {string} id - The identifier of the transaction.
+ * @property {string} docDateTime - The date and time of the transaction document.
+ * @property {number} totalSum - The total sum of the transaction.
+ * @property {number} cashSum - The cash sum of the transaction.
+ * @property {number} eCashSum - The electronic cash sum of the transaction.
+ * @property {number} creditSum - The credit sum of the transaction.
+ *
+ * @example
+ * const ofdDto = new OfdDto({
+ *   Id: '123',
+ *   DocDateTime: '2023-01-01T00:00:00',
+ *   TotalSumm:  100,
+ *   CashSumm:  50,
+ *   ECashSumm:  50,
+ *   CreditSumm:  0
+ * });
  */
 export class OfdDto {
-    /**
-     * Unique identifier for the OFD record.
-     */
     id;
-
-    /**
-     * Total sum of transaction included in the OFD record.
-     */
+    docDateTime;
     totalSum;
-
-    /**
-     * Sum of cash transaction included in the OFD record.
-     */
     cashSum;
-
-    /**
-     * Sum of electronic cash transaction included in the OFD record.
-     */
     eCashSum;
-
-    /**
-     * Sum of credit card transaction included in the OFD record.
-     */
     creditSum;
 
     /**
-     * Constructs a new instance of the OfdDto class.
+     * Constructs an OfdDto instance from a given object.
      *
-     * @param {Object} obj - The object containing the OFD record data.
-     * @param {number} obj.Id - The unique identifier for the OFD record.
-     * @param {number} obj.TotalSumm - The total sum of transaction.
-     * @param {number} obj.CashSumm - The sum of cash transaction.
-     * @param {number} obj.ECashSumm - The sum of electronic cash transaction.
-     * @param {number} obj.CreditSumm - The sum of credit card transaction.
+     * @param {Object} obj - The object containing transaction data.
+     * @param {string} obj.Id - The identifier of the transaction.
+     * @param {string} obj.DocDateTime - The date and time of the transaction document.
+     * @param {number} obj.TotalSumm - The total sum of the transaction.
+     * @param {number} obj.CashSumm - The cash sum of the transaction.
+     * @param {number} obj.ECashSumm - The electronic cash sum of the transaction.
+     * @param {number} obj.CreditSumm - The credit sum of the transaction.
      */
     constructor(obj) {
         this.id = obj['Id'];
+        this.docDateTime = obj['DocDateTime']
         this.totalSum = obj['TotalSumm'];
         this.cashSum = obj['CashSumm'];
         this.eCashSum = obj['ECashSumm'];
